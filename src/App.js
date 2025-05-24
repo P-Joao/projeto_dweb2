f5fd7c9c88989eb3338902d24ef2753ccb464d3c
P-Joao <rjoaopedro2020@gmail.com>
2025-05-24 17:42:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import Tabela from './components/tabela';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    filmes: []
+  };
+  componentDidMount() {
+    fetch("../api/filmes.json")
+      .then(response => response.json())
+      .then(filmes => this.setState({ filmes }))
+      .catch(function (error) {
+        console.log("Erro na requisição")
+      })
+  }
+  handlerExcluirLinha = imdb_id => {
+    const filmes = this.state.filmes.filter(filme => filme.imdb_id !== imdb_id);
+    this.setState({ filmes });
+  };
+  handlerOrdernarCrescente = titulo => {
+    const filmes = this.state.filmes.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    this.setState({ filmes });
+  }
+  handlerOrdernarDecrescente = titulo => {
+    const filmes = this.state.filmes.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    filmes.reverse();
+    this.setState({ filmes });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <Tabela
+          filmes={this.state.filmes}
+          excluirLinha={this.handlerExcluirLinha}
+          ordernarCrescente={this.handlerOrdernarCrescente}
+          ordernarDecrescente={this.handlerOrdernarDecrescente}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
